02f04557200f1865b81565b9783f4c45b54b5ebe
shanecglass <23001651+shanecglass@users.noreply.github.com>
2022-12-16 15:46:47 -0500

    Updated sync to GH file

--- political_ads/sync-gcs-to-gh.py
+++ political_ads/sync-gcs-to-gh.py
@@ -1,34 +1,90 @@
+# Triggered by a change in a storage bucket
+
 import base64
-from github import Github
-from github import InputGitTreeElement
+import os
+import shutil
+import logging
+import tempfile
+import functions_framework
+import git
+from os.path import exists
+from google.cloud import storage
+from google.cloud.storage.blob import Blob
+from git import Repo
 
-user = "shanecglass"
-password = "*********"
-g = Github(user,password)
-repo = g.get_user().get_repo('git-test') # repo name
-file_list = [
-    'C:\\Users\jesse\Dropbox\Swell-Forecast\git-test\index.html',
-    'C:\\Users\jesse\Dropbox\Swell-Forecast\git-test\margin_table.html'
-]
-file_names = [
-    'index.html',
-    'margin_table.html'
-]
-commit_message = 'python commit'
-master_ref = repo.get_git_ref('heads/master')
-master_sha = master_ref.object.sha
-base_tree = repo.get_git_tree(master_sha)
+@functions_framework.cloud_event
+def hello_gcs(cloud_event):
+  data = cloud_event.data
 
-element_list = list()
-for i, entry in enumerate(file_list):
-    with open(entry) as input_file:
-        data = input_file.read()
-    if entry.endswith('.png'): # images must be encoded
-        data = base64.b64encode(data)
-    element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
-    element_list.append(element)
+  event_id = cloud_event["id"]
+  event_type = cloud_event["type"]
 
-tree = repo.create_git_tree(element_list, base_tree)
-parent = repo.get_git_commit(master_sha)
-commit = repo.create_git_commit(commit_message, tree, [parent])
-master_ref.edit(commit.sha)
+  bucket = data["bucket"]
+  file_name = data["name"]
+  metageneration = data["metageneration"]
+  timeCreated = data["timeCreated"]
+  updated = data["updated"]
+
+  print(f"Event ID: {event_id}")
+  print(f"Event type: {event_type}")
+  print(f"Bucket: {bucket}")
+  print(f"File: {file_name}")
+  print(f"Metageneration: {metageneration}")
+  print(f"Created: {timeCreated}")
+  print(f"Updated: {updated}")
+
+  if "parquet" in file_name:
+    if "output-00000" in file_name:
+      file_name = (file_name.split("/",2)[1])
+    else:
+      print("Oh, uh, nevermind. File name is already right")
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(file_name)
+    folder = "/tmp/repo"
+    if os.path.exists(folder):
+      print("No worries, the folder exists")
+    else:
+      os.mkdir(folder)
+    short_name = os.path.basename(blob.name)
+    logging.info("Blobs: {}".format(short_name))
+    destination_uri = folder + "/" + short_name
+
+    repo_dir = "/tmp/repo"
+    if os.path.exists(repo_dir):
+      shutil.rmtree(repo_dir, ignore_errors=True)
+      os.mkdir(repo_dir)
+    else:
+      os.mkdir(repo_dir)
+    user_email = "23001651+shanecglass@users.noreply.github.com"
+    user_name = "shanecglass"
+    password = os.environ.get("GITHUB-PAT")
+    print("Auth information and file to commit defined")
+
+    remote_repo = f"https://{user_name}:{password}@github.com/shanecglass/malloy_demos.git"
+    print(remote_repo)
+    Repo.clone_from(remote_repo, repo_dir, branch = "main")
+    repo = git.Repo(repo_dir)
+    print("repo cloned")
+
+    destination_folder = folder + "/political_ads"
+    destination_uri = destination_folder + "/"+ short_name
+    if os.path.exists(destination_folder):
+      blob.download_to_filename(destination_uri)
+    else:
+      os.mkdir(destination_folder)
+      blob.download_to_filename(destination_uri)
+    logging.info("Exported {} to {}".format(blob, destination_uri))
+    print("File downloaded")
+
+    file_to_commit = short_name
+    file_path_to_commit = destination_uri
+    commit_message = 'Updating data'
+    repo.index.add(file_path_to_commit)
+    repo.index.commit(commit_message)
+    origin = repo.remote(name="origin")
+    origin.push()
+    print("Commit complete")
+
+  else:
+    print("These are intermediate files. We're ignoring them")
